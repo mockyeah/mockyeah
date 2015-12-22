3a7f74c90851fd24bf91e3e1ac2d9ef12f5c3570
Ryan Ricard <ryan.ricard@gmail.com>
2015-12-21 20:12:11 -0600

    Add tdd watch gulp task
    - Add watch task to run source linter on script file change.

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,19 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
+const paths = {
+  scripts: ['!node_modules/**', './**/*.js']
+};
+
 gulp.task('lint', function lint() {
-  return gulp.src(['./**/*.js', '!node_modules/**'])
+  return gulp.src(paths.scripts)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
+});
+
+gulp.task('tdd', function tdd() {
+  gulp.watch(paths.scripts, ['lint']);
 });
 
 gulp.task('default', ['lint']);
