bce3c3727e5ddb447c1753047910fe6e6ecc23d4
Ryan Ricard <ryan.ricard@gmail.com>
2015-12-21 20:08:58 -0600

    Add gulp and default task
    - Add default task to execule source linter

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+gulp.task('lint', function lint() {
+  return gulp.src(['./**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['lint']);
