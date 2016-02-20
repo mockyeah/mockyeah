6b31e80370122c2e9b320738b7b194055a2cfc5c
Ryan Ricard <ryan.ricard@gmail.com>
2016-02-20 17:40:43 -0600

    Add `gulp test:watch` task
    - Replaces previous `gulp tdd` task
    - Added test options to control when gulp exits. This allows gulp to continuously run tests with watcher while still exiting with failure for continuous integration.

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@
 /* eslint-disable no-process-exit */
 
 const gulp = require('gulp');
+const gutil = require('gulp-util');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 
 const paths = {
   scripts: ['!node_modules/**', './**/*.js'],
-  tests: ['./test/**/*-test.js']
+  tests: ['./test/**/*Test.js']
 };
 
 gulp.task('lint', function lint() {
@@ -18,30 +19,37 @@ gulp.task('lint', function lint() {
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('test', () => {
+function test(options) {
+  options = options || {
+    exit: true
+  };
+
   return gulp.src(paths.tests)
     .pipe(mocha())
-    // Explicitly exits to avoid hanging
-    .once('error', () => {
-      process.exit(1);
+    .once('error', function (err) {
+      gutil.log(err);
+
+      if(options.exit) { // if configured to exit, exit with a failure code
+        process.exit(1);
+      } else { // else, emit 'end' event to avoid exiting
+        this.emit('end');
+      }
     })
     .once('end', () => {
-      process.exit();
+      if(options.exit) process.exit();
     });
+}
+
+gulp.task('test', function () {
+  return test();
 });
 
-/**
- * Special `tdd-test` task exists due to `test` needing to explicitly exit
- */
-gulp.task('tdd-test', () => {
-  return gulp.src(paths.tests)
-    .pipe(mocha({
-      reporter: 'dot'
-    }));
+gulp.task('test:watch:run', function () {
+  return test({ exit: false });
 });
 
-gulp.task('tdd', function tdd() {
-  gulp.watch(paths.scripts, ['lint', 'tdd-test']);
+gulp.task('test:watch', ['test:watch:run'], function () {
+  gulp.watch(paths.scripts, ['test:watch:run']);
 });
 
 gulp.task('default', ['lint']);
