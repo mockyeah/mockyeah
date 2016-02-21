165b9edb6797e3320685270a75c68e464e726061
Ryan Ricard <ryan.ricard@gmail.com>
2016-02-20 21:31:10 -0600

    Set test global constants

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -1,8 +1,8 @@
 'use strict';
 
 global.MOCKYEAH_ROOT = __dirname;
-global.MOCKYEAH_SUPPRESS_OUTPUT = false;
-global.MOCKYEAH_VERBOSE_OUTPUT = true;
+global.MOCKYEAH_SUPPRESS_OUTPUT = true;
+global.MOCKYEAH_VERBOSE_OUTPUT = false;
 
 const mockyeah = require('../index.js');
 const request = require('supertest');
