775a447bd1e81dd9b23dc1e736e3965422083035
Sebastian Mitterle <sebastianmitterle@mathume.com>
2016-10-31 20:44:02 +0100

    added route overwriting

--- app/lib/RouteStore.js
+++ app/lib/RouteStore.js
@@ -15,8 +15,17 @@ function RouteStore(app) {
 
 RouteStore.prototype.register = function register(method, path, response) {
   const route = { method, path, response };
+  this.unregisterRouteIfAlreadyPresent(route);
   this.routeResolver.register(route);
   this.routes.push(route);
+};
+
+RouteStore.prototype.unregisterRouteIfAlreadyPresent = function unregisterRouteIfAlreadyPresent(route){
+  var alreadyPresent = this.routes.filter((r) => r.method == route.method && r.path == route.path);
+  if(alreadyPresent){
+    this.routes.splice(alreadyPresent[0], 1);
+  }
+  this.routeResolver.unregister([route]);
 };
 
 RouteStore.prototype.reset = function reset() {
--- test/integration/ServerTest.js
+++ test/integration/ServerTest.js
@@ -2,15 +2,34 @@
 
 const TestHelper = require('../TestHelper');
 const request = TestHelper.request;
+const mockyeah = TestHelper.mockyeah;
 
 describe('Server', () => {
-  it('should respond to root http requests', (done) => {
-    request
-      .get('/')
-      .expect(200, /Hello\, mockyeah\!/, done);
-  });
+    it('should respond to root http requests', (done) => {
+        request
+            .get('/')
+            .expect(200, /Hello\, mockyeah\!/, done);
+    });
 
-  it('should respond with a 404 for unknown paths', (done) => {
-    request.get('/unknown/path').expect(404, done);
-  });
+    it('should respond with a 404 for unknown paths', (done) => {
+        request.get('/unknown/path').expect(404, done);
+    });
+
+    it('should set a definition', (done) => {
+        mockyeah.get('/test', {text: "someValue", status: 401});
+        request
+            .get('/test')
+            .expect(401, "someValue", done);
+    });
+
+    it('should overwrite an existing definition', (done) => {
+        mockyeah.get('/test', {text: "someValue", status: 401});
+        request
+            .get('/test').then(() => {
+                mockyeah.get('/test', {text: "someOtherValue", status: 200});
+                request
+                    .get('/test')
+                    .expect(200, "someOtherValue", done);
+            });
+    });
 });
