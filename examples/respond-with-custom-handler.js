9f9d5760dfbdcb0db681443d25b32fb0e5c5d810
Ryan Ricard <ryan.ricard@gmail.com>
2017-03-17 12:10:59 -0500

    Add respond with custom handler example

---
+++ examples/respond-with-custom-handler.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const log = require('./log');
+const mockyeah = require('./mockyeah');
+
+mockyeah.get('/', (req, res, next) => {
+  res.send('I\'m an Express.js route! Customize to your heart\'s content.');
+});
+
+http.get('http://localhost:4001', (res) => {
+  log(res);
+  mockyeah.close();
+});
