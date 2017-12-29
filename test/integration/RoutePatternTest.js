24a52dbfb7cfc316c65c920f9cab8b0e42cc7128
Anders D. Johnson <AndersDJohnson@users.noreply.github.com>
2017-12-29 15:47:49 -0600

    fix: restore request params for custom middleware (#55)
    * fix: restore request params for custom middleware
    
    * Update RoutePatternTest.js
    
    * Update RoutePatternTest.js

--- test/integration/RoutePatternTest.js
+++ test/integration/RoutePatternTest.js
@@ -3,12 +3,54 @@
 const TestHelper = require('../TestHelper');
 const mockyeah = TestHelper.mockyeah;
 const request = TestHelper.request;
+const expect = require('chai').expect;
 
 describe('Route Patterns', () => {
-  it('should work with route parameter', done => {
+  it('should work with path parameter', done => {
     mockyeah.get('/service/:key');
 
     request.get('/service/exists').expect(200, done);
+  });
+
+  it('should expose path parameters to custom middleware as keyed object', done => {
+    let report = true;
+    mockyeah.get('/service/:one/:two/other/:three', (req, res) => {
+      try {
+        expect(req.params).to.deep.equal({
+          one: 'exists',
+          two: 'ok',
+          three: 'yes',
+          0: 'exists',
+          1: 'ok',
+          2: 'yes'
+        });
+      } catch (err) {
+        done(err);
+        report = false;
+      }
+      res.send();
+    });
+
+    request.get('/service/exists/ok/other/yes').expect(200, () => {
+      if (report) done();
+    });
+  });
+
+  it('should expose path parameters to custom middleware as indexed array', done => {
+    let report = true;
+    mockyeah.get('/service/:one/:two/other/:three', (req, res) => {
+      try {
+        expect(req.params[1]).to.equal('ok');
+      } catch (err) {
+        done(err);
+        report = false;
+      }
+      res.send();
+    });
+
+    request.get('/service/exists/ok/other/yes').expect(200, () => {
+      if (report) done();
+    });
   });
 
   it('should work with regular expression slash any count', done => {
