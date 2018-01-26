mockyeah/mockyeah

commit 2626c66b8945cdeddc5c0297e9bfa531e8eed487

refactor: use generic route handler with matching (#46)
* refactor: use generic route handler with matching

This refactors mockyeah to use a generic route handler (all *),
and match against mocked routes within that handler. This will enable
future work to support matching on things other than path,
such as query parameters, headers, body, etc., similar to nock.

* refactor: simplify route to replace conditional

* bump

app/index.js
@@ -41,14 +41,14 @@ module.exports = function App(config) {
   // Provide user feedback when verbose output is enabled
   app.log('info', 'verbose output enabled', true);
 
-  // Attach RouteManager to app object, the primary set of mockyeah API methods.
-  app.routeManager = new RouteManager(app);
-
-  app.use(bodyParser.json());
-
   app.get('/', (req, res) => {
     res.send('Hello, mockyeah!');
   });
+
+  app.use(bodyParser.json());
+
+  // Attach RouteManager to app object, the primary set of mockyeah API methods.
+  app.routeManager = new RouteManager(app);
 
   return app;
 };

app/lib/RouteManager.js
@@ -45,7 +45,7 @@ module.exports = function RouteManager(app) {
 
     record: function record(captureName) {
       const capture = new CaptureRecorder(app, captureName);
-      this.register('all', '*', capture.record.bind(capture));
+      app.use(capture.record.bind(capture));
     },
 
     play: function play(captureName) {

app/lib/RouteResolver.js
@@ -2,9 +2,34 @@
 
 const fs = require('fs');
 const path = require('path');
+const parse = require('url').parse;
 const _ = require('lodash');
+const pathToRegExp = require('path-to-regexp');
 const expandPath = require('../../lib/expandPath');
 const Expectation = require('./Expectation');
+
+function isEqualMethod(method1, method2) {
+  const m1 = method1.toLowerCase();
+  const m2 = method2.toLowerCase();
+  return m1 === 'all' || m2 === 'all' || m1 === m2;
+}
+
+function isRouteForRequest(route, req) {
+  if (!isEqualMethod(req.method, route.method)) return false;
+
+  const pathname = parse(req.url, true).pathname;
+
+  if (route.pathname !== '*' && !route.pathRegExp.test(pathname)) return false;
+
+  // TODO: Later add features to match other things, like query parameters, etc.
+
+  return true;
+}
+
+function isRouteToReplace(newRoute, oldRoute) {
+  return newRoute.pathname === oldRoute.pathname && newRoute.method === oldRoute.method;
+}
+
 /**
  * RouteResolver
  *  Facilitates route registration and unregistration.
@@ -12,6 +37,10 @@ const Expectation = require('./Expectation');
  */
 function RouteResolver(app) {
   this.app = app;
+
+  this.routes = [];
+
+  this.listening = false;
 }
 
 function validateResponse(response) {
@@ -120,12 +149,18 @@ RouteResolver.prototype.register = function register(route) {
     route.response = handler.call(this, route.response);
   }
 
+  route.pathname = parse(route.path, true).pathname;
+  route.pathRegExp = pathToRegExp(route.pathname);
+
   const expectation = new Expectation(route);
+  route.expectation = expectation;
 
   // unregister existing matching routes
   this.unregister([route]);
 
-  this.app[route.method](route.path, expectation.middleware.bind(expectation), route.response);
+  this.routes.push(route);
+
+  this.listen();
 
   return {
     expect: () => expectation.api()
@@ -133,10 +168,35 @@ RouteResolver.prototype.register = function register(route) {
 };
 
 RouteResolver.prototype.unregister = function unregister(routes) {
-  this.app._router.stack = this.app._router.stack.filter((layer) => {
-    return !(layer.route && routes.some((route) => {
-      return route.path === layer.route.path && layer.route.methods[route.method] === true;
-    }));
+  // Filter out any old routes that match any of the new routes being unregistered.
+  this.routes = this.routes.filter(oldRoute =>
+    !routes.some(newRoute => isRouteToReplace(newRoute, oldRoute))
+  );
+};
+
+RouteResolver.prototype.listen = function listen() {
+  if (this.listening) return;
+
+  this.listening = true;
+
+  this.app.all('*', (req, res, next) => {
+    const route = this.routes.find(r => isRouteForRequest(r, req));
+
+    if (!route) {
+      next();
+      return;
+    }
+
+    const expectationNext = err => {
+      if (err) {
+        this.app.log(['record', 'expectation', 'error'], err);
+        res.sendStatus(500);
+        return;
+      }
+      route.response(req, res);
+    };
+
+    route.expectation.middleware(req, res, expectationNext);
   });
 };
 

test/integration/RoutePatternTest.js
@@ -1,0 +1,47 @@
+'use strict';
+
+const TestHelper = require('../TestHelper');
+const mockyeah = TestHelper.mockyeah;
+const request = TestHelper.request;
+
+describe('Route Patterns', () => {
+  it('should work with route parameter', (done) => {
+    mockyeah.get('/service/:key');
+
+    request
+      .get('/service/exists')
+      .expect(200, done);
+  });
+
+  it('should work with regular expression slash any count', (done) => {
+    mockyeah.get('/service/(.{0,})');
+
+    request
+      .get('/service/exists')
+      .expect(200, done);
+  });
+
+  it('should work with regular expression slash any star', (done) => {
+    mockyeah.get('/(.*)');
+
+    request
+      .get('/service/exists')
+      .expect(200, done);
+  });
+
+  it('should work with regular expression any star', (done) => {
+    mockyeah.get('(.*)');
+
+    request
+      .get('/service/exists')
+      .expect(200, done);
+  });
+
+  it('should work with star', (done) => {
+    mockyeah.get('*');
+
+    request
+      .get('/service/exists')
+      .expect(200, done);
+  });
+});

commit 9316ef0e1084899e08c21bb637720bb1a74e45de

fix: no fake root path
This was interfering with users mounting responses on the root '/' path.
Broken since #46.

app/index.js
@@ -46,16 +46,14 @@ module.exports = function App(config) {
   // Provide user feedback when verbose output is enabled
   app.log('info', 'verbose output enabled', true);
 
-  app.get('/', (req, res) => {
-    res.send('Hello, mockyeah!');
-  });
-
   app.use(bodyParser.json());
 
   app.middlewares = [];
 
   app.use((req, res, next) => {
-    async.series(app.middlewares.map(middleware => cb => middleware(req, res, cb)), err => next(err));
+    async.series(app.middlewares.map(middleware => cb => middleware(req, res, cb)), err =>
+      next(err)
+    );
   });
 
   app.use = middleware => {

test/integration/ServerTest.js
@@ -4,10 +4,6 @@ const TestHelper = require('../TestHelper');
 const request = TestHelper.request;
 
 describe('Server', () => {
-  it('should respond to root http requests', done => {
-    request.get('/').expect(200, /Hello\, mockyeah\!/, done);
-  });
-
   it('should respond with a 404 for unknown paths', done => {
     request.get('/unknown/path').expect(404, done);
   });