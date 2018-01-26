63bb51b4903aad3f6627821ae0118dc39350b5e3
AndersDJohnson <AndersDJohnson@users.noreply.github.com>
2018-01-26 11:06:31 -0600

    fix: normalize path trailing slashes

--- app/lib/RouteResolver.js
+++ app/lib/RouteResolver.js
@@ -12,10 +12,19 @@ function isEqualMethod(method1, method2) {
   return m1 === 'all' || m2 === 'all' || m1 === m2;
 }
 
+const justSlashes = /^\/+$/;
+const trailingSlashes = /\/+$/;
+
+function normalizePathname(pathname) {
+  if (!pathname || justSlashes.test(pathname)) return '/';
+  // remove any trailing slashes
+  return pathname.replace(trailingSlashes, '');
+}
+
 function isRouteForRequest(route, req) {
   if (!isEqualMethod(req.method, route.method)) return false;
 
-  const pathname = parse(req.url, true).pathname;
+  const pathname = normalizePathname(parse(req.url, true).pathname);
 
   if (route.pathname !== '*' && !route.pathRegExp.test(pathname)) return false;
 
@@ -78,7 +87,7 @@ function RouteResolver(app) {
 RouteResolver.prototype.register = function register(method, path, response) {
   const route = { method, path, response };
 
-  route.pathname = parse(route.path, true).pathname;
+  route.pathname = normalizePathname(parse(route.path, true).pathname);
   const matchKeys = [];
   route.pathRegExp = pathToRegExp(route.pathname, matchKeys);
   route.matchKeys = matchKeys;
