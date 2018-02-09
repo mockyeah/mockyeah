25da1cce8f2f761903dbb6f516da7db623af5517
Anders D. Johnson <AndersDJohnson@users.noreply.github.com>
2018-02-09 09:12:36 -0600

    feat: mocks by query & body + object syntax (#67)
    * feat: match mocks based on query parameters
    
    * feat: support object match syntax
    
    * fix: replace routes with query parameter matches
    
    * fix: sync query null in string vs. object match
    
    * Update RoutePatternTest.js
    
    * fix: whitespace issue
    
    * WIP
    
    * feat: request body matching
    
    * docs: TODO check body with other content types

--- test/integration/RoutePatternTest.js
+++ test/integration/RoutePatternTest.js
@@ -86,6 +86,14 @@ describe('Route Patterns', () => {
     });
   });
 
+  it('should allow match object', done => {
+    mockyeah.get({
+      path: '/foo'
+    });
+
+    request.get('/foo').expect(200, done);
+  });
+
   it('should work with path parameter', done => {
     mockyeah.get('/service/:key');
 
@@ -155,5 +163,103 @@ describe('Route Patterns', () => {
     mockyeah.get('*');
 
     request.get('/service/exists').expect(200, done);
+  });
+
+  it('should fail when doesnt match query parameters', done => {
+    mockyeah.get('/foo?bar=yes');
+
+    request.get('/foo').expect(404, done);
+  });
+
+  it('should match single query parameter', done => {
+    mockyeah.get('/foo?bar=yes');
+
+    request.get('/foo?bar=yes').expect(200, done);
+  });
+
+  it('should match single query parameter with object', done => {
+    mockyeah.get({
+      path: '/foo',
+      query: {
+        bar: 'yes'
+      }
+    });
+
+    request.get('/foo?bar=yes').expect(200, done);
+  });
+
+  it('should match multiple query parameters', done => {
+    mockyeah.get('/foo?bar=yes&cool=duh');
+
+    request.get('/foo?bar=yes&cool=duh').expect(200, done);
+  });
+
+  it('should match multiple query parameters with object', done => {
+    mockyeah.get({
+      path: '/foo',
+      query: {
+        bar: 'yes',
+        cool: 'duh'
+      }
+    });
+
+    request.get('/foo?bar=yes&cool=duh').expect(200, done);
+  });
+
+  it('should match list query parameters', done => {
+    mockyeah.get('/foo?bar=yes&list=a&list=b');
+
+    request.get('/foo?bar=yes&list=a&list=b').expect(200, done);
+  });
+
+  it('should match list query parameters with object', done => {
+    mockyeah.get({
+      path: '/foo',
+      query: {
+        bar: 'yes',
+        list: ['a', 'b']
+      }
+    });
+
+    request.get('/foo?bar=yes&list=a&list=b').expect(200, done);
+  });
+
+  it('should match request body', done => {
+    mockyeah.post({
+      path: '/foo',
+      body: {
+        bar: 'yes'
+      }
+    });
+
+    request
+      .post('/foo')
+      .send({ bar: 'yes' })
+      .expect(200, done);
+  });
+
+  it('should fail to match when different request body', done => {
+    mockyeah.post({
+      path: '/nope',
+      body: {
+        bar: 'nope'
+      }
+    });
+
+    request
+      .post('/nope')
+      .send({ bar: 'yes' })
+      .expect(404, done);
+  });
+
+  it('should fail to match when no request body', done => {
+    mockyeah.post({
+      path: '/nope',
+      body: {
+        bar: 'nope'
+      }
+    });
+
+    request.post('/nope').expect(404, done);
   });
 });
