dbe37419ce92638874a9d627bdd3e259165643bf
Anders D. Johnson <AndersDJohnson@users.noreply.github.com>
2018-02-12 21:16:39 -0600

    feat: support optional https (#72)
    * feat: support optional https
    
    * refactor: common listen method

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 'use strict';
 
+const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
+const createCertFiles = require('create-cert-files');
+const { partial } = require('lodash');
 const App = require('../app');
 const prepareConfig = require('../lib/prepareConfig');
-const cors = require('cors');
+
 /**
  * Server module
  * @param  {Object} config Application configuration.
@@ -17,13 +22,44 @@ module.exports = function Server(config, onStart) {
   // Enable CORS for all routes
   app.use(cors());
 
-  // Start server on conigured hose and port
-  const server = app.listen(config.port, config.host, function listen() {
-    this.rootUrl = `http://${this.address().address}:${this.address().port}`;
+  function listen(secure, err) {
+    if (err) throw err;
+    this.rootUrl = `http${secure ? 's' : ''}://${this.address().address}:${this.address().port}`;
     app.log('serve', `Listening at ${this.rootUrl}`);
     // Execute callback once server starts
     if (onStart) onStart.call(this);
-  });
+  }
+
+  // Start server on configured host and port
+  let server;
+
+  if (config.portHttps) {
+    let certFiles;
+    if (!config.httpsKeyPath && !config.httpsCertPath) {
+      certFiles = createCertFiles();
+    } else {
+      certFiles = {
+        key: config.httpsKeyPath,
+        cert: config.httpsCertPath
+      };
+    }
+
+    /* eslint-disable no-sync */
+    const key = fs.readFileSync(certFiles.key);
+    const cert = fs.readFileSync(certFiles.cert);
+    /* eslint-enable no-sync */
+
+    const credentials = {
+      key,
+      cert
+    };
+
+    const httpsServer = https.createServer(credentials, app);
+
+    server = httpsServer.listen(config.portHttps, config.host, partial(listen, true));
+  } else {
+    server = app.listen(config.port, config.host, partial(listen, false));
+  }
 
   // Expose ability to stop server via API
   const close = function close(cb) {
