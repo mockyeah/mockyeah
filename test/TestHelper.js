181702c7efc9918ab1671d6031474feb500b98e2
Anders D. Johnson <AndersDJohnson@users.noreply.github.com>
2018-02-12 21:16:39 -0600

    feat: support optional https (#72)
    * feat: support optional https
    
    * refactor: common listen method

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -1,5 +1,7 @@
 'use strict';
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 global.MOCKYEAH_ROOT = __dirname;
 global.MOCKYEAH_SUPPRESS_OUTPUT = true;
 global.MOCKYEAH_VERBOSE_OUTPUT = false;
@@ -7,13 +9,22 @@ global.MOCKYEAH_VERBOSE_OUTPUT = false;
 const mockyeah = require('../index.js');
 const request = require('supertest');
 
+const configHttps = Object.assign({}, mockyeah.config, {
+  portHttps: 4443
+});
+const mockyeahHttps = new mockyeah.Server(configHttps);
+
 after(() => {
   mockyeah.close();
+  mockyeahHttps.close();
 });
 
 afterEach(() => {
   mockyeah.reset();
+  mockyeahHttps.reset();
 });
 
 exports.mockyeah = mockyeah;
+exports.mockyeahHttps = mockyeahHttps;
 exports.request = request(mockyeah.server);
+exports.requestHttps = request(mockyeahHttps.server);
