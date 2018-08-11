c2261d13350c91b29b5143e851e16dac05317a57
Ryan Ricard <ryan.ricard@gmail.com>
2018-08-10 20:18:18 -0500

    modify handling of `match` option to allow being passed as param

--- app/recorder.js
+++ app/recorder.js
@@ -1,24 +1,29 @@
 const UrlPattern = require('url-pattern');
 
 module.exports = app => (name, options = {}) => {
-  const { match } = options;
-  let matchFn = () => true;
+  let { match } = options;
 
   app.locals.recording = true;
   if (!name) throw new Error('Must provide a recording name.');
 
   app.log(['serve', 'record'], name);
 
-  if (match) {
+  if (typeof match === 'string') {
+    // if match is a string, assume it is a url pattern
     const urlPattern = new UrlPattern(match);
     app.log(['serve', 'record', 'match'], urlPattern.stringify());
-    matchFn = urlPattern.match.bind(urlPattern);
+    match = urlPattern.match.bind(urlPattern);
+  }
+
+  if (!match) {
+    // if match is not defined, assign noop which matches all requests
+    match = () => true;
   }
 
   app.locals.recordMeta = {
     name,
     options,
-    match: matchFn
+    match
   };
 
   // Store whether we're proxying so we can reset it later.
