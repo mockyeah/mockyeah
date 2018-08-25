669b7a71c47401ef6b6b305b2b630812a2b7f45c
AndersDJohnson <AndersDJohnson@users.noreply.github.com>
2018-08-25 16:57:13 -0500

    test: add unit tests for prepare config

---
+++ packages/mockyeah/test/unit/prepareConfig.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const { expect } = require('chai');
+
+const prepareConfig = require('../../lib/prepareConfig');
+
+describe('prepareConfig', () => {
+  it('should work and use defaults with no config input', () => {
+    const config = prepareConfig();
+    expect(config).to.deep.equal({
+      adminHost: 'localhost',
+      adminPort: 4777,
+      adminProtocol: 'http',
+      adminServer: true,
+      capturesDir: '/Users/anders/code/mockyeah/mockyeah',
+      fixturesDir: '/Users/anders/code/mockyeah/fixtures',
+      host: 'localhost',
+      httpsCertPath: undefined,
+      httpsKeyPath: undefined,
+      journal: false,
+      name: 'mockyeah',
+      output: true,
+      port: 4001,
+      proxy: false,
+      record: false,
+      verbose: false
+    });
+  });
+});
