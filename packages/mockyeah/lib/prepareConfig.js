8adc77b2e3ce6aea2262889f243ab16dc6c3b621
Anders D. Johnson <AndersDJohnson@users.noreply.github.com>
2018-09-11 23:31:39 -0500

    refactor: no need for "or" config fallback

--- packages/mockyeah/lib/prepareConfig.js
+++ packages/mockyeah/lib/prepareConfig.js
@@ -24,7 +24,7 @@ module.exports = (config = {}) => {
   config.adminHost = config.adminHost || config.host || configDefaults.host;
   config.proxy = (config.proxy || config.record) === true;
 
-  config = Object.assign({}, configDefaults, config || {});
+  config = Object.assign({}, configDefaults, config);
 
   // Expand file system configuration paths relative to configuration root
   config.fixturesDir = expandPath(config.fixturesDir);
