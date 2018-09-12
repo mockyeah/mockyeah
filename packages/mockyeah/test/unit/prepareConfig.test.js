63896490e4ac6bfeb0659462d7b1ffa3e66e116b
AndersDJohnson <AndersDJohnson@users.noreply.github.com>
2018-09-11 23:16:55 -0500

    test: add for empty config

--- packages/mockyeah/test/unit/prepareConfig.test.js
+++ packages/mockyeah/test/unit/prepareConfig.test.js
@@ -26,4 +26,25 @@ describe('prepareConfig', () => {
       verbose: false
     });
   });
+  it('should work and use defaults with empty config input', () => {
+    const config = prepareConfig({});
+    expect(config).to.deep.equal({
+      adminHost: 'localhost',
+      adminPort: 4777,
+      adminProtocol: 'http',
+      adminServer: true,
+      capturesDir: '/Users/anders/code/mockyeah/mockyeah',
+      fixturesDir: '/Users/anders/code/mockyeah/fixtures',
+      host: 'localhost',
+      httpsCertPath: undefined,
+      httpsKeyPath: undefined,
+      journal: false,
+      name: 'mockyeah',
+      output: true,
+      port: 4001,
+      proxy: false,
+      record: false,
+      verbose: false
+    });
+  });
 });
