fbf8ac5d899aa5d935ec93bbdd8d7f67f2c96a42
AndersDJohnson <AndersDJohnson@users.noreply.github.com>
2019-01-12 14:39:26 -0600

    WIP

--- packages/mockyeah/index.js
+++ packages/mockyeah/index.js
@@ -5,5 +5,11 @@
 const Server = require('./server');
 const config = require('./config');
 
+/* eslint-disable */
+if (true === true) {
+  console.log('OK');
+}
+/* eslint-enable */
+
 module.exports = new Server(config);
 module.exports.Server = Server;
