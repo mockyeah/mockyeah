b9f93211ef52ba3970d19131e6ba6b5885bb0730
AndersDJohnson <anders.d.johnson.developer@gmail.com>
2019-01-14 21:25:15 -0600

    WIP

--- packages/mockyeah/app/lib/matches.js
+++ packages/mockyeah/app/lib/matches.js
@@ -1,45 +1,25 @@
 const { isMatchWith, isRegExp } = require('lodash');
 
-const SKIP = {};
-
 // eslint-disable-next-line consistent-return
-function customizer(object, source, skip) {
+function customizer(object, source) {
   if (isRegExp(source)) {
-    if (!source.test(object)) throw new Error(`value "${object}" does not match regex "${source}"`);
-    if (skip === SKIP) return SKIP;
+    return source.test(object);
   } else if (typeof source === 'number') {
-    if (source.toString() !== object)
-      throw new Error(`number "${source}" does not match "${object}"`);
-    if (skip === SKIP) return SKIP;
+    return source.toString() === object;
   } else if (typeof source === 'function') {
     const result = source(object);
     // if the function returns undefined, we'll skip this to fallback
-    if (result !== undefined && !result)
-      throw new Error(
-        `value ${object} does not pass function${source.name ? ` "${source.name}"` : ''}`
-      );
-    if (skip === SKIP) return SKIP;
+    if (result !== undefined) return result;
   }
   // else return undefined to fallback to default equality check
 }
 
-// eslint-disable-next-line consistent-return
-const matches = (object, source, options = {}) => {
-  const { throws } = options;
+const matches = (object, source) => {
+  const result = customizer(object, source);
 
-  try {
-    const result = customizer(object, source, SKIP);
+  if (result !== undefined) return result;
 
-    if (!throws && result === SKIP) return true;
-
-    isMatchWith(object, source, customizer);
-
-    if (!throws) return true;
-  } catch (err) {
-    if (throws) throw err;
-
-    return false;
-  }
+  return isMatchWith(object, source, customizer);
 };
 
 module.exports = matches;
