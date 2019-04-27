e7f81fd635f5a501dd42a309a3211ca9e421cf14
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2019-04-27 10:34:27 -0500

    WIP

--- packages/mockyeah/test/integration/ClosePromiseTest.js
+++ packages/mockyeah/test/integration/ClosePromiseTest.js
@@ -7,10 +7,16 @@ describe('close() promise', () => {
   it('should use a promise', () => {
     const mockyeah = new MockYeahServer({ port: 0, adminPort: 0 });
     return mockyeah.close()
-  })
+  });
 
   it('should use a promise even when given a callback', () => {
     const mockyeah = new MockYeahServer({ port: 0, adminPort: 0 });
     return mockyeah.close(() => { })
-  })
+  });
+
+  it('should reject if close fails', done => {
+    const mockyeah = new MockYeahServer({ port: 0, adminPort: 0 });
+    mockyeah.server.close = cb => cb(new Error('fail'));
+    mockyeah.close().then(() => done(new Error('expected error'))).catch(() => done());
+  });
 });
