6ed069463c8347a195f5fedbb5fbb76f69eda38b
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2019-05-06 00:46:07 -0500

    refactor(cli): move bin/index to bin/mockyeah (#372)
    * refactor(cli): move bin/index to bin/mockyeah
    
    * WIP
    
    * WIP

---
+++ packages/mockyeah-cli/bin/mockyeah.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * mockyeah cli api "table of contents".
+ * Root `mockyeah` command does not invoke behavior other than
+ * providing user with a catalog of available actions.
+ */
+
+const program = require('commander');
+const boot = require('../lib/boot');
+const version = require('../version');
+
+boot(() => {
+  program
+    .name('mockyeah')
+    .version(version)
+    .command('ls', 'list suites')
+    .command('play [suiteNames]', 'play suite(s)')
+    .command('playAll', 'play all suites')
+    .command('record [suiteName]', 'record suite')
+    .command('start', 'start server')
+    .parse(process.argv);
+});
