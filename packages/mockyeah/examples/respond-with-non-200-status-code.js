d9217a87f728f62ac2e7c98aa70774ad6de6ba9b
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2019-09-09 07:02:11 -0500

    refactor: move examples into mockyeah package (#430)

---
+++ packages/mockyeah/examples/respond-with-non-200-status-code.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const log = require('./log');
+const mockyeah = require('./mockyeah');
+
+mockyeah.get('/', { text: 'bummer.', status: 500 });
+
+http.get('http://localhost:4001', res => {
+  log(res);
+  mockyeah.close();
+});
