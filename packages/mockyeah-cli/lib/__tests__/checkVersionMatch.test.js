d928bda7445a91aab77fb854a21cebd4baf0b322
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2019-11-15 23:17:16 -0600

    feat(cli): version mismatch uses semver (#460)

--- packages/mockyeah-cli/lib/__tests__/checkVersionMatch.test.js
+++ packages/mockyeah-cli/lib/__tests__/checkVersionMatch.test.js
@@ -24,36 +24,138 @@ describe('checkVerisonMatch', () => {
 
     test('throws on mismatch package versions', () => {
       expect(() => {
+        const pkgUp = {
+          package: {
+            version: '0.0.0-test'
+          }
+        };
         const env = {
           modulePackage: {
             version: '99.99.99-test'
           }
         };
+        checkVersionMatch(env, pkgUp);
+      }).toThrow(
+        'Version mismatch between @mockyeah/cli@0.0.0-test and @mockyeah/server@99.99.99-test - please install compatible versions.'
+      );
+    });
+
+    test('passes on identical package versions', () => {
+      expect(() => {
         const pkgUp = {
           package: {
             version: '0.0.0-test'
           }
         };
-        checkVersionMatch(env, pkgUp);
-      }).toThrow(
-        'Version mismatch between CLI (0.0.0-test) and core (99.99.99-test) - please install same versions.'
-      );
-    });
-
-    test('passes on matching package versions', () => {
-      expect(() => {
         const env = {
           modulePackage: {
             version: '0.0.0-test'
           }
         };
+        checkVersionMatch(env, pkgUp);
+      }).not.toThrow();
+    });
+
+    test('passes on identical package versions', () => {
+      expect(() => {
         const pkgUp = {
           package: {
             version: '0.0.0-test'
           }
         };
+        const env = {
+          modulePackage: {
+            version: '0.0.0-test'
+          }
+        };
         checkVersionMatch(env, pkgUp);
       }).not.toThrow();
+    });
+
+    test('passes on different prerelease package versions', () => {
+      expect(() => {
+        const pkgUp = {
+          package: {
+            version: '0.0.1-alpha.1'
+          }
+        };
+        const env = {
+          modulePackage: {
+            version: '0.0.1-alpha.2'
+          }
+        };
+        checkVersionMatch(env, pkgUp);
+      }).not.toThrow();
+    });
+
+    test('passes on different patch package versions', () => {
+      expect(() => {
+        const env = {
+          modulePackage: {
+            version: '0.0.1'
+          }
+        };
+        const pkgUp = {
+          package: {
+            version: '0.0.2'
+          }
+        };
+        checkVersionMatch(env, pkgUp);
+      }).not.toThrow();
+    });
+
+    test('throws on different minor package versions', () => {
+      expect(() => {
+        const pkgUp = {
+          package: {
+            version: '0.1.0'
+          }
+        };
+        const env = {
+          modulePackage: {
+            version: '0.2.0'
+          }
+        };
+        checkVersionMatch(env, pkgUp);
+      }).toThrow(
+        'Version mismatch between @mockyeah/cli@0.1.0 and @mockyeah/server@0.2.0 - please install compatible versions.'
+      );
+    });
+
+    test('throws on different major package versions', () => {
+      expect(() => {
+        const pkgUp = {
+          package: {
+            version: '1.0.0'
+          }
+        };
+        const env = {
+          modulePackage: {
+            version: '2.0.0'
+          }
+        };
+        checkVersionMatch(env, pkgUp);
+      }).toThrow(
+        'Version mismatch between @mockyeah/cli@1.0.0 and @mockyeah/server@2.0.0 - please install compatible versions.'
+      );
+    });
+
+    test('throws on different major package versions with prerelease', () => {
+      expect(() => {
+        const pkgUp = {
+          package: {
+            version: '1.0.0'
+          }
+        };
+        const env = {
+          modulePackage: {
+            version: '2.0.0-alpha.0'
+          }
+        };
+        checkVersionMatch(env, pkgUp);
+      }).toThrow(
+        'Version mismatch between @mockyeah/cli@1.0.0 and @mockyeah/server@2.0.0-alpha.0 - please install compatible versions.'
+      );
     });
   });
 });
