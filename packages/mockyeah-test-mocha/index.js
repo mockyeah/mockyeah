72cd469e72a6b78030034fba574cc8dbc38c7fd1
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2019-12-20 23:00:00 -0600

    fix: test packages no websocket

--- packages/mockyeah-test-mocha/index.js
+++ packages/mockyeah-test-mocha/index.js
@@ -1,7 +1,9 @@
 require('isomorphic-fetch');
 const Mockyeah = require('@mockyeah/fetch');
 
-const mockyeah = new Mockyeah();
+const mockyeah = new Mockyeah({
+  noWebSocket: true
+});
 
 afterEach(() => mockyeah.reset());
 
