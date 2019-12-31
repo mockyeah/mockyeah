23905c2064a3e6ec2492f50c67edc3f6c25a140a
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2019-12-31 16:08:12 -0600

    test(fetch): add for parse query from url in object (#505)

--- packages/mockyeah-fetch/src/__tests__/normalize.test.ts
+++ packages/mockyeah-fetch/src/__tests__/normalize.test.ts
@@ -9,8 +9,37 @@ describe('normalize', () => {
     });
   });
 
+  test('parse query', () => {
+    expect(
+      normalize({
+        url: '/v1?ok=yes'
+      })
+    ).toMatchObject({
+      query: {
+        ok: 'yes'
+      }
+    });
+  });
+
   test('original normal', () => {
     expect(normalize('/v1?ok=yes')).toMatchObject({
+      $meta: {
+        originalNormal: {
+          url: '/v1',
+          query: {
+            ok: 'yes'
+          }
+        }
+      }
+    });
+  });
+
+  test('original normal with query from url in object', () => {
+    expect(
+      normalize({
+        url: '/v1?ok=yes'
+      })
+    ).toMatchObject({
       $meta: {
         originalNormal: {
           url: '/v1',
