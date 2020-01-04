b93b1108ed352068265f703d88bffe7218d2fc8e
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2020-01-04 03:17:52 -0600

    format

--- packages/mockyeah-docs/gatsby-config.js
+++ packages/mockyeah-docs/gatsby-config.js
@@ -49,10 +49,7 @@ module.exports = {
         // GitHub Flavored Markdown mode (default: true)
         gfm: true,
         // Plugins configs
-        plugins: [
-          `gatsby-remark-autolink-headers`,
-          `gatsby-remark-prismjs`
-        ]
+        plugins: [`gatsby-remark-autolink-headers`, `gatsby-remark-prismjs`]
       }
     },
     {
