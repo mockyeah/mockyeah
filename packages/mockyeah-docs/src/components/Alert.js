e25cc8b87d6343499f6784d2cc044127fa46de04
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2020-01-05 12:31:50 -0600

    alert

---
+++ packages/mockyeah-docs/src/components/Alert.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Alert = ({ children }) => {
+  return (
+    <div style={{ background: '#fff3cd', color: '#856404', border: '1px solid #ffeeba', padding: 12 }}>
+      {children}
+    </div>
+  )
+};
+
+export { Alert };
