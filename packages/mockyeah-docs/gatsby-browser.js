4c9eab8a4ad806c6e61c5d4c962284828ee8f9e5
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2020-01-05 11:43:04 -0600

    feat(docs): add alert for mockyeah 0.x docs

--- packages/mockyeah-docs/gatsby-browser.js
+++ packages/mockyeah-docs/gatsby-browser.js
@@ -4,13 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 import React from 'react';
-import { Link } from 'gatsby';
 import { MDXProvider } from '@mdx-js/react';
 import 'prismjs/themes/prism.css';
 
-const components = {
-  a: ({ href, ...props }) => <Link to={href} {...props} />
-};
+const components = {};
 
 export const wrapRootElement = ({ element }) => (
   <MDXProvider components={components}>{element}</MDXProvider>
