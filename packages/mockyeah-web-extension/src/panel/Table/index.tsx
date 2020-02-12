9570fba4455fd696b0ac974b921e994a2dc47d34
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2020-02-12 01:45:53 -0600

    feat(webext): search network table

--- packages/mockyeah-web-extension/src/panel/Table/index.tsx
+++ packages/mockyeah-web-extension/src/panel/Table/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useTable } from "react-table";
+import React from "react";
+import { useTable, useFilters, useGlobalFilter } from "react-table";
 
 const Table = ({ columns, data }) => {
   // Use the state and functions returned from useTable to build your UI
@@ -7,53 +7,71 @@ const Table = ({ columns, data }) => {
     getTableProps,
     getTableBodyProps,
     headerGroups,
+    prepareRow,
+    state,
     rows,
-    prepareRow
-  } = useTable({
-    columns,
-    data
-  });
+    preGlobalFilteredRows,
+    setGlobalFilter
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useFilters,
+    useGlobalFilter
+  );
 
   if (!columns) return null;
   if (!data) return null;
 
   // Render the UI for your table
   return (
-    <table {...getTableProps()} style={{ width: "100%" }}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th
-                {...column.getHeaderProps()}
-                className={`table-head-${column.id}`}
-              >
-                {column.render("Header")}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
-                    className={`table-cell-${cell.column.id}`}
-                  >
-                    {cell.render("Cell")}
-                  </td>
-                );
-              })}
+    <div>
+      <span>
+        Search:{" "}
+        <input
+          value={state.globalFilter || ""}
+          onChange={e => {
+            setGlobalFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+          }}
+        />
+      </span>
+      <table {...getTableProps()} style={{ width: "100%" }}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th
+                  {...column.getHeaderProps()}
+                  className={`table-head-${column.id}`}
+                >
+                  {column.render("Header")}
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      className={`table-cell-${cell.column.id}`}
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
